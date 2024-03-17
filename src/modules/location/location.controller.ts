d6b89a736f9eabe4d41ace70f82564ed865c7398
TungLam Phan Dam <126652156+tunglampd@users.noreply.github.com>
2024-03-17 17:39:03 +0700

    implement location

--- src/modules/location/location.controller.ts
+++ src/modules/location/location.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, Res } from '@nestjs/common';
+import { AuthenticatedRequest } from 'src/helper/common/interfaces';
+import { CreateLocationDto } from './dtos/create-location.dto';
+import { UpdateLocationDto } from './dtos/update-location.dto';
+import { LocationService } from './location.service';
 
 @Controller('location')
-export class LocationController {}
+export class LocationController {
+    constructor(
+        private locationService: LocationService,
+    ) { }
+
+    // Get all locations by user
+    @Get()
+    async getLocationsByUser(@Res() res: any, @Req() req: AuthenticatedRequest) {
+        const result = await this.locationService.getLocationsByUser(req.user)
+        res.status(result.status).json(result.content)
+    }
+
+    // Create location by user
+    @Post()
+    async addLocation(@Res() res: any, @Req() req: AuthenticatedRequest, @Body() createLocationDto: CreateLocationDto) {
+        const result = await this.locationService.addLocation(req.user, createLocationDto)
+        res.status(result.status).json(result.content)
+    }
+
+    // Delete location by user
+    @Delete(':id')
+    async deleteLocation(@Res() res: any, @Req() req: AuthenticatedRequest, @Param('id') id: string) {
+        const result = await this.locationService.deleteLocation(req.user, id)
+        res.status(result.status).json(result.content)
+    }
+
+    // Update location by user
+    @Put()
+    async updateLocation(@Res() res: any, @Req() req: AuthenticatedRequest, @Body() updateLocationDto: UpdateLocationDto) {
+        const result = await this.locationService.updateLocation(req.user, updateLocationDto)
+        res.status(result.status).json(result.content)
+    }
+
+    // Update location by user
+    @Get(':id')
+    async getLocation(@Res() res: any, @Req() req: AuthenticatedRequest, @Param('id') id: string) {
+        const result = await this.locationService.getLocation(req.user, id)
+        res.status(result.status).json(result.content)
+    }
+}
--- src/modules/location/location.module.ts
+++ src/modules/location/location.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
 
 @Module({
   controllers: [LocationController],
-  providers: [LocationService]
+  providers: [LocationService],
+  imports: [
+    TypeOrmModule.forFeature([Location, User]),
+  ]
 })
-export class LocationModule {}
+export class LocationModule { }
